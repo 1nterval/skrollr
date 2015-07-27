4b2b976384a4cd20b752f56fc32036bb259f2eb0
Fabien Quatravaux <fabien.quatravaux@1nterval.com>
2015-07-27 15:18:24 +0200

    [fix] Translate the "Browse Happy" message

--- header.php
+++ header.php
@@ -1,4 +1,11 @@
 	<!--[if lt IE 7]>
-		<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+		<p class="browsehappy"><?php 
+			printf( 
+				wp_kses( __( 'You are using an <strong>outdated</strong> browser. Please <a href="%s">upgrade your browser</a> to improve your experience', 'skrollr' ),
+					array( 'a' => array( 'href' => array() ), 'strong' => array() )
+				),
+				esc_url( 'http://browsehappy.com/' )
+			);
+		?></p>
 	<![endif]-->
 
